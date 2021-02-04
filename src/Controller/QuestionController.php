b730fe4a980530ae688453caa5276d8138ceb8a7
Vedran Milković <vedran.milkovic25@gmail.com>
2021-02-04 18:13:01 +0100

    Add cache and change markdown parser config

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController {
 
@@ -23,7 +24,7 @@ class QuestionController extends AbstractController {
     /**
      *  @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -32,11 +33,14 @@ class QuestionController extends AbstractController {
         ];
 
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
-        $pasedQuestionText = $markdownParser->transformMarkdown($questionText);
+
+        $parseQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $pasedQuestionText,
+            'questionText' => $parseQuestionText,
             'answers' => $answers
         ]);
     }
