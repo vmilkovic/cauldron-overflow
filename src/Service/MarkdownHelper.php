598b9b6de2a7bd3d2bb93ec3014a6b2a3bc83712
Vedran Milković <vedran.milkovic25@gmail.com>
2021-02-06 21:30:17 +0100

    Add MarkdownHelper service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -7,15 +7,22 @@ use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class MarkdownHelper {
 
-    private $markdownParser;
-    private $cache;
+    private MarkdownParserInterface $markdownParser;
+    private CacheInterface $cache;
+    private bool $isDebug;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache){
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug){
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        dump($isDebug);
     }
 
     public function parse(string $source): string {
+
+        if(!$this->isDebug){
+            return $this->markdownParser->transformMarkdown($source);
+        }
 
         return $this->cache->get('markdown_'.md5($source), function() use ($source) {
             return $this->markdownParser->transformMarkdown($source);
