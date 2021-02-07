3cc81115e92928ae4ec245f9f8e10c1313b11ce3
Vedran Milković <vedran.milkovic25@gmail.com>
2021-02-07 19:02:47 +0100

    Add Sentry error reporting and secrets container

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Twig\Environment;
 use Psr\Log\LoggerInterface;
 use App\Service\MarkdownHelper;
+use Sentry\State\HubInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,7 +34,7 @@ class QuestionController extends AbstractController {
      *  @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug, MarkdownHelper $markdownHelper){
-
+        
         if($this->isDebug){
             $this->logger->info('We are in debug mode!');
         }
